ff8e163292aa755934fa6a5416aab06ce3056b3b
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2021-12-12 17:43:15 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -90,7 +90,6 @@ async def text_(client: Client, message: Message):
                 ],
             ]
         ),
-        disable_web_page_preview=False,
     )
 @Client.on_message(
     command(["alive", f"alive@{BOT_USERNAME}"]) & filters.private & ~filters.edited
